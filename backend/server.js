be496be56d47201da13f792ddce6b8af47d49c17
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2026-05-07 18:37:19 +0530

    Updated production backend URL

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/payment", paymentRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
