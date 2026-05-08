c0b4dcc31c3a70118bdbbd688c9d056fe490e987
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2026-05-08 09:35:30 +0530

    Fixed backend product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,80 +4,78 @@ const router = express.Router();
 
 const Product = require("../models/Product");
 
+// ADD PRODUCT
+
 router.post("/add", async (req, res) => {
-
-    try {
-
-        const newProduct = new Product(req.body);
-
-        await newProduct.save();
-
-        res.status(201).json({
-            success: true,
-            message: "Product Added",
-            product: newProduct
-        });
-
-    } catch (error) {
-
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-
-    }
-
-});
-
-router.get("/", async (req, res) => {
-
-    try {
-
-        const products = await Product.find();
-
-        res.status(200).json({
-            success: true,
-            products
-        });
-
-    } catch (error) {
-
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-
-    }
-    
-    // DELETE PRODUCT
-
-router.delete("/:id", async (req, res) => {
 
   try {
 
-    await Product.findByIdAndDelete(
-      req.params.id
-    );
+    const newProduct = new Product(req.body);
 
-    res.status(200).json({
+    await newProduct.save();
 
+    res.status(201).json({
       success: true,
-      message: "Product Deleted"
-
+      message: "Product Added",
+      product: newProduct
     });
 
   } catch (error) {
 
     res.status(500).json({
-
       success: false,
       message: error.message
-
     });
 
   }
 
 });
+
+// GET PRODUCTS
+
+router.get("/", async (req, res) => {
+
+  try {
+
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      products
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+
+});
+
+// DELETE PRODUCT
+
+router.delete("/:id", async (req, res) => {
+
+  try {
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Product Deleted"
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
 
 });
 
