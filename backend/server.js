d4d285df65237446d7a9fa4083a9219bd1b2ada2
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2026-05-13 10:34:08 +0530

    Added auth routes

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,23 @@ require("dotenv").config();
 const chatRoutes = require("./routes/chatRoutes");
 const productRoutes = require("./routes/productRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors({
-  origin: ["https://khushboo-enterprises.vercel.app", "http://localhost:3000"],
+  origin: [
+    "https://khushboo-enterprises.vercel.app",
+    "http://localhost:3000"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Routes
+
 app.get("/", (req, res) => {
   res.send("Backend Working");
 });
@@ -24,13 +30,16 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/payment", paymentRoutes);
+app.use("/api/auth", authRoutes);
 
 // MongoDB Connection
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
